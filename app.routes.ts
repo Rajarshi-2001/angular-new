912864cc84aac79a771496cd4d9348b246fcd7ac
Rajarshi-2001 <121023938+Rajarshi-2001@users.noreply.github.com>
2025-05-29 14:19:26 +0530

    Update app.routes.ts

--- app.routes.ts
+++ app.routes.ts
@@ -1,7 +1,6 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NewAppComponent } from './new-app/new-app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { NgModule } from '@angular/core';
 
 export const routes: Routes = [{path:'',component:NewAppComponent},{path:'dashboard',component:DashboardComponent}];
 export class AppRoutingModule { }
