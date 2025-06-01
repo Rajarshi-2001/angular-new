8ac5960476684da37e94b2cf03764e74678cf472
Rajarshi-2001 <121023938+Rajarshi-2001@users.noreply.github.com>
2025-06-01 23:02:34 +0530

    Update new-app.component.ts

--- new-app.component.ts
+++ new-app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { PopupComponent } from '../popup/popup.component';
+import { Router } from '@angular/router';
+import { DashboardComponent } from '../dashboard/dashboard.component';
 
 @Component({
   selector: 'app-new-app',
@@ -14,8 +16,19 @@ export class NewAppComponent {
   username: string = '';
   password: string='';
   showPopup: boolean = false;
-
+  constructor (private router:Router){};
   onLogin() {
-    this.showPopup = true;
+    if(this.username== "admin" && this.password=="admin"){
+      this.showPopup = true;
+      this.router.navigate(["./dashboard"]);
+    }else {
+      alert('Invalid username or password!');
+      this.showPopup = false;
+    }
+    
+    
+  }
+  Logout(){
+    this.showPopup= false;
   }
 }
